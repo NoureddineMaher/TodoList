5790b1758179bef46a20931ab1eb225d8d3fe6ca
ettaoussiilyas <75787782+ettaoussiilyas@users.noreply.github.com>
2024-06-30 19:27:26 +0100

    last things commet

--- server/index.js
+++ server/index.js
@@ -6,13 +6,19 @@ const persistdb = require('./config/persistdb')
 persistdb();
 const projectRouter = require('./routers/projectRouter')
 
+const taskRouters = require('./routers/taskRouter')
+
 const cors = require('cors');
-app.use(cors({origin:true,credentials:true}));
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+
 
 
 app.use('/api', projectRouter);
+app.use(cors({credentials: true,}))
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use('/api',taskRouters)
+
 app.listen(PORT , () => {
     console.log(`server connected at port: ${PORT}`)
 });
